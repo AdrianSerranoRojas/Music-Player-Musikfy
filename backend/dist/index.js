a086095b89d068bdfce1da9a071080f99e848a7a
Adrián Serrano <adrianno_3@hotmail.com>
2022-04-22 15:17:46 +0200

    TS-node config

---
+++ backend/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const server_js_1 = __importDefault(require("./server.js"));
+server_js_1.default.listen(4000, () => console.log("servidor funcionando de una puta vez!"));
+//# sourceMappingURL=index.js.map
---
+++ backend/dist/server.js
@@ -0,0 +1,13 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const helmet_1 = __importDefault(require("helmet"));
+const morgan_1 = __importDefault(require("morgan"));
+const app = express_1.default();
+app.use(morgan_1.default("dev"));
+app.use(helmet_1.default());
+exports.default = app;
+//# sourceMappingURL=server.js.map
