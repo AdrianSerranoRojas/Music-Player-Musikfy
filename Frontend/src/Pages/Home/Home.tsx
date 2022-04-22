e81f52ccefe9553c49540b94921906a23517011f
Adrián Serrano <adrianno_3@hotmail.com>
2022-04-22 11:01:54 +0200

    Hoc, home

---
+++ Frontend/src/Pages/Home/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import withLayout from "../../hoc/withLayout";
+
+
+const Home = () => {
+
+  return (
+    <div>
+      HOME
+    </div>
+  );
+};
+
+export default withLayout(Home);
