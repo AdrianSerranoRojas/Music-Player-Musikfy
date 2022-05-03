8d74e1229100300efcb3d2376a806595d80848a3
RB <87038824+MrMhirs@users.noreply.github.com>
2022-05-03 13:55:17 +0200

    Full navBar

--- Frontend/src/components/sideBar/SideBar.tsx
+++ Frontend/src/components/sideBar/SideBar.tsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import "./SideBar.scss";
 
-import PermIdentityOutlinedIcon from '@mui/icons-material/PermIdentityOutlined';
-import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
-import PlaylistAddOutlinedIcon from '@mui/icons-material/PlaylistAddOutlined';
-import AlternateEmailOutlinedIcon from '@mui/icons-material/AlternateEmailOutlined';
-import VpnKeyOutlinedIcon from '@mui/icons-material/VpnKeyOutlined';
+import PermIdentityOutlinedIcon from "@mui/icons-material/PermIdentityOutlined";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import PlaylistAddOutlinedIcon from "@mui/icons-material/PlaylistAddOutlined";
+import AlternateEmailOutlinedIcon from "@mui/icons-material/AlternateEmailOutlined";
+import VpnKeyOutlinedIcon from "@mui/icons-material/VpnKeyOutlined";
 
 import { SideBarData } from "./SideBarData";
 import { Link } from "react-router-dom";
@@ -16,15 +16,15 @@ function SideBar() {
 
   return (
     <div className="slideBar">
-      <div className="navBar">
-      </div>
-      <nav className={slideBar ? "navMenu" : "navMenu"}>
+      <nav className="navMenu">
+        <div className="logo">
+          <img
+            src="https://res.cloudinary.com/dhqzvelnb/image/upload/v1651072060/Logo/MusikfyLogo-green_ysbv6t.png"
+            alt="profile image"
+          ></img>
+        </div>
         <ul className="navMenuItems">
-            <Link to="#" className="menuBars">
-            </Link>
-
-
-
+          <Link to="#" className="menuBars"></Link>
           {SideBarData.map((item, index) => {
             return (
               <li key={index} className={item.cName}>
@@ -35,6 +35,13 @@ function SideBar() {
               </li>
             );
           })}
+        <div className="ProfileImg2">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4SwRYqnvSzUO1XJzADbOWDjoouV0nqZOv2w&usqp=CAU"
+            alt="User image"
+            className="ProfileImg"
+          ></img>
+        </div>
         </ul>
       </nav>
     </div>
@@ -42,3 +49,4 @@ function SideBar() {
 }
 
 export default SideBar;
+
