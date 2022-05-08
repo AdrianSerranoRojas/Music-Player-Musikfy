e3cf91fde82a7adaf8eb551190805fb57d9bce92
RoyerMateu <rogermateu2001@gmail.com>
2022-05-08 20:30:42 +0200

    home style

--- Frontend/src/Pages/Home/Home.tsx
+++ Frontend/src/Pages/Home/Home.tsx
@@ -14,6 +14,7 @@ import Box from "@mui/material/Box";
 import PlaylistsCard from "../../components/MainCards/PlaylistsCard/PlaylistsCard";
 
 import "./Home.scss"
+import { styled } from "@mui/material/styles";
 
 async function fetchUserToken(
   setUserToken: any,
@@ -30,6 +31,52 @@ async function fetchUserToken(
     setLoading(false);
   }
 }
+
+const WallPaper = styled("div")({
+  position: "absolute",
+  width: "100%",
+  height: "100%",
+  top: 0,
+  left: 0,
+  overflow: "hidden",
+  background: "linear-gradient(rgb(255, 38, 142) 0%, rgb(255, 105, 79) 100%)",
+  transition: "all 500ms cubic-bezier(0.175, 0.885, 0.32, 1.275) 0s",
+  "&:before": {
+      content: '""',
+      width: "140%",
+      height: "140%",
+      position: "absolute",
+      top: "-40%",
+      right: "-50%",
+      background:
+      "radial-gradient(at center center, rgb(62, 79, 249) 0%, rgba(62, 79, 249, 0) 64%)",
+  },
+  "&:after": {
+  content: '""',
+  width: "140%",
+  height: "140%",
+  position: "absolute",
+  bottom: "-50%",
+  left: "-30%",
+  background:
+      "radial-gradient(at center center, rgb(247, 237, 225) 0%, rgba(247, 237, 225, 0) 70%)",
+  transform: "rotate(30deg)",
+  },
+});
+
+const Widget = styled("div")(({ theme }) => ({
+  padding: 16,
+  borderRadius: 16,
+  width: 830,
+  maxWidth: "100%",
+  margin: "auto",
+  position: "relative",
+  zIndex: 1,
+  backgroundColor:
+    theme.palette.mode === "dark" ? "rgba(0,0,0,0.6)" : "rgba(255,255,255,0.4)",
+  backdropFilter: "blur(40px)",
+}));
+
 const Home = () => {
   const [file, setFile] = useState(null);
 
@@ -67,24 +114,21 @@ const Home = () => {
       >
         click
       </button> */}
-      <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)', ml: 10, mr:10,  gap: 5 }}>
-        <Box sx={{
-          bgcolor: 'white',
+      <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)', ml: 10, mr:10,  gap: 5}}>
+        <Widget sx={{
           boxShadow: 4,
-          borderRadius: 1,
           p: 2}}>
           <LikesCard/>
-        </Box>
-        <Box sx={{
-          bgcolor: 'white',
+        </Widget>
+        <Widget sx={{
           boxShadow: 4,
-          borderRadius: 1,
           p: 2,
           pl: 6}}>
           <PlaylistsCard />
-        </Box>
+        </Widget>
       </Box>
       <MusicPlayer />
+      <WallPaper />
     </div>
   );
 };
