d773be3a4582c65b8c84b470160518706ef9b59f
Adrián Serrano <adrianno_3@hotmail.com>
2022-05-09 19:43:19 +0200

    create song With AUTH

--- Frontend/src/services/songApi.tsx
+++ Frontend/src/services/songApi.tsx
@@ -1,12 +1,26 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { getCurrentUserToken } from "../firebase/firebase";
 
 // Define a service using a base URL and expected endpoints
 export const songApi = createApi({
   reducerPath: "songApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:4000" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:4000",
+    prepareHeaders: async (headers, { getState }) => {
+      const userToken = await getCurrentUserToken();
+      // If we have a token set in state, let's assume that we should be passing it.
+      if (userToken) {
+        headers.set("authorization", `Bearer ${userToken}`);
+      }
+      return headers;
+    },
+  }),
   endpoints: (builder) => ({
     getSongs: builder.query({
       query: () => `/songs`,
+    }),
+    getMySongs: builder.query({
+      query: (userId) => `/songs/${userId}`,
     }),
     createSong: builder.mutation({
       query: (body) => ({
