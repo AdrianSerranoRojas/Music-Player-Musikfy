20cbf1fcc52af95ab270ca2092d15f84ad4f651b
Adrián Serrano <adrianno_3@hotmail.com>
2022-05-09 13:54:18 +0200

    upload Song WIP

--- Frontend/src/services/songApi.tsx
+++ Frontend/src/services/songApi.tsx
@@ -1,17 +1,23 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // Define a service using a base URL and expected endpoints
 export const songApi = createApi({
-  reducerPath: 'songApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:4000' }),
+  reducerPath: "songApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:4000" }),
   endpoints: (builder) => ({
     getSongs: builder.query({
       query: () => `/songs`,
     }),
+    createSong: builder.mutation({
+      query: (body) => ({
+        url: `/songs`,
+        method: "POST",
+        body,
+      }),
+    }),
   }),
-})
+});
 
 // Export hooks for usage in functional components, which are
 // auto-generated based on the defined endpoints
-export const { useGetSongsQuery } = songApi;
+export const { useGetSongsQuery, useCreateSongMutation } = songApi;
