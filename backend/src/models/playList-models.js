5855024720c85ccd65dcabcccaf77bc9400ecec5
gcachonc <g.cachon.c@gmail.com>
2022-05-12 13:59:10 +0200

    trying mutation playlist

--- backend/src/models/playList-models.js
+++ backend/src/models/playList-models.js
@@ -39,64 +39,64 @@ const PlaylistSchema = new Schema(
       minLength: [2, "Playlist title needs to have at least 2 characters"],
       maxLength: [50, "Playlist title cannot exceed 50 characters"],
     },
-    description: {
-      type: String,
-      required: [true, "Please enter description to the playlist"],
-      trim: true,
-      minLength: [
-        2,
-        "Playlist description needs to have at least 2 characters",
-      ],
-      maxLength: [200, "Playlist description cannot exceed 200 characters"],
-    },
-    songs: {
-      type: [{ type: Schema.Types.ObjectId, ref: "Song" }],
-      default: [],
-    },
-    genre: {
-      type: String,
-      enum: {
-        values: [
-          "",
-          "Country",
-          "Electronic dance music (EDM)",
-          "Hip-hop",
-          "Indie rock",
-          "Jazz",
-          "K-pop",
-          "Metal",
-          "Oldies",
-          "Pop",
-          "Rap",
-          "Rhythm & blues (R&B)",
-          "Rock",
-          "Techno",
-          "Folk",
-          "Ska",
-          "Reggae",
-          "Punk",
-        ],
-      },
-    },
-    private: {
-      type: Boolean,
-      required: true,
-    },
-    owner: {
-      type: String,
-      // ref: "User",
-      ref: "users",
-      required: true,
-    },
-    playlistImage: {
-      type: String,
-      default:
-        "https://res.cloudinary.com/carapolla/image/upload/v1652290253/playlistsPicture/estilos_f8zdsl.jpg",
-    },
-    likes: {
-      type: Number,
-      default: 0,
-    },
+  //   description: {
+  //     type: String,
+  //     required: [true, "Please enter description to the playlist"],
+  //     trim: true,
+  //     minLength: [
+  //       2,
+  //       "Playlist description needs to have at least 2 characters",
+  //     ],
+  //     maxLength: [200, "Playlist description cannot exceed 200 characters"],
+  //   },
+  //   songs: {
+  //     type: [{ type: Schema.Types.ObjectId, ref: "Song" }],
+  //     default: [],
+  //   },
+  //   genre: {
+  //     type: String,
+  //     enum: {
+  //       values: [
+  //         "",
+  //         "Country",
+  //         "Electronic dance music (EDM)",
+  //         "Hip-hop",
+  //         "Indie rock",
+  //         "Jazz",
+  //         "K-pop",
+  //         "Metal",
+  //         "Oldies",
+  //         "Pop",
+  //         "Rap",
+  //         "Rhythm & blues (R&B)",
+  //         "Rock",
+  //         "Techno",
+  //         "Folk",
+  //         "Ska",
+  //         "Reggae",
+  //         "Punk",
+  //       ],
+  //     },
+  //   },
+  //   private: {
+  //     type: Boolean,
+  //     required: true,
+  //   },
+  //   owner: {
+  //     type: String,
+  //     // ref: "User",
+  //     ref: "users",
+  //     required: true,
+  //   },
+  //   playlistImage: {
+  //     type: String,
+  //     default:
+  //       "https://res.cloudinary.com/carapolla/image/upload/v1652290253/playlistsPicture/estilos_f8zdsl.jpg",
+  //   },
+  //   likes: {
+  //     type: Number,
+  //     default: 0,
+  //   },
   },
   { timestamps: true }
 );
