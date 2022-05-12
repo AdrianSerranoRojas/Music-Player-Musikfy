e64f41a379bc3f22c21ee3e2c5fda1c6d3899997
Adrián Serrano <adrianno_3@hotmail.com>
2022-05-12 20:07:36 +0200

    redirect login logout WIP

--- Frontend/src/components/AddSongForm/TemplateForm.tsx
+++ Frontend/src/components/AddSongForm/TemplateForm.tsx
@@ -1,50 +1,95 @@
-import { useContext } from "react";
+import { useState, useCallback, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
-import { FcGoogle } from "react-icons/fc";
 
 import { Formik, Form } from "formik";
-
-import Button from "@mui/material/Button";
+import * as Yup from "yup";
 import SelectField from "../../components/SelectField/SelectField";
 import SelectFieldMulty from "../../components/SelectFieldMulty/SelectFieldMulty";
+
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import { Button } from "@mui/material";
+import { CardMedia } from "@mui/material";
 import { TextField } from "@mui/material";
 
-import Box from "@mui/material/Box";
+import { useDropzone } from "react-dropzone";
 
-import AuthContext from "../../context/AuthContext";
+// import useData from "./useData";
 
-import * as Yup from "yup";
+import { useDispatch } from "react-redux";
+import { addSongFile, updateSongFile } from "../../features/song/songsSlice";
+import { useCreateSongMutation } from "../../services/songApi";
 
 const songSchema = Yup.object().shape({
-  userName: Yup.string()
-    .min(2, "The userName is too short!")
-    .max(50, "The userName is too long!")
-    .required("The password is required"),
-  finder: Yup.string()
-    .min(2, "The userName is too short!")
-    .max(50, "The userName is too long!")
-    .required("The password is required"),
+  files: Yup.mixed().required(),
+  songName: Yup.string()
+    .min(2, "The songName is too short!")
+    .max(50, "The songName is too long!"),
+  songArtist: Yup.string()
+    .min(2, "The songArtist is too short!")
+    .max(50, "The songArtist is too long!"),
 });
+
+const optionsGenre = [
+  { value: "Rock", label: "Rock" },
+  { value: "Rap", label: "Rap" },
+  { value: "Pop", label: "Pop" },
+];
+
+const initialValues = {
+  files: null,
+  songName: "",
+  songArtist: "",
+  genre: "",
+  songAlbum: "",
+};
+
+const UploadComponent = (props) => {
+  const { setFieldValue, setImage } = props;
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    accept: {
+      "audio/*": [".mp3"],
+    },
+    onDrop: (acceptedFiles) => {
+      acceptedFiles.forEach((file) => {
+        const reader = new FileReader();
+        reader.onload = () => {
+          setImage((prevState) => [...prevState, reader.result]);
+        };
+        reader.readAsDataURL(file);
+        console.log("el reader", reader);
+      });
+      setFieldValue("files", acceptedFiles);
+    },
+  });
+  return (
+    <div>
+      {}
+      <div {...getRootProps({ className: "dropzone" })}>
+        <input {...getInputProps()} />
+        {isDragActive ? (
+          <p>Drop the files here ...</p>
+        ) : (
+          <p>Drag 'n' drop some files here, or click to select files</p>
+        )}
+      </div>
+    </div>
+  );
+};
 
 function AddSongForm() {
   let signUpError = false;
+  const [image, setImage] = useState([]);
+  const [createSong, result] = useCreateSongMutation();
 
-  const currentUser = useContext(AuthContext);
-
-  const optionsGenre = [
-    { value: "Rock", label: "Rock" },
-    { value: "Rap", label: "Rap" },
-    { value: "Pop", label: "Pop" },
-  ];
-
-  const handleSubmit = (values) => {
+  const handleSubmit = async (values) => {
     console.log(values);
+    await createSong(image);
   };
 
-  const initialValues = {
-    userName: "",
-    finder: "",
-    genre: "",
+  const insertFile = async () => {
+    await createSong(image);
   };
 
   return (
@@ -52,7 +97,6 @@ function AddSongForm() {
       <Box sx={{ display: "flex", alignItems: "center" }}>
         <h1 className="h1 h1SingUp">Upload Song</h1>
       </Box>
-
       <section className="row row-cols-1">
         <div className="col">
           <h2 className="h5 mb-3">upload your own songs</h2>
@@ -66,6 +110,7 @@ function AddSongForm() {
             validationSchema={songSchema}
           >
             {({
+              setFieldValue,
               handleChange,
               handleSubmit,
               submitForm,
@@ -77,91 +122,80 @@ function AddSongForm() {
             }) => (
               <Form>
                 <div className="grid1">
-                  <Box
-                    sx={{
-                      display: "grid",
-                      gridTemplateColumns: { sm: "1fr 1fr" },
-                      gap: 2,
-                      marginTop: 2,
-                    }}
-                  >
-                    <TextField
-                      id="userName"
-                      name="userName"
-                      label="user Name"
-                      value={values.userName}
-                      onChange={handleChange}
-                      error={touched.userName && Boolean(errors.userName)}
-                      helperText={touched.userName && errors.userName}
+                  {/* dropzone */}
+                  <div className="form-group">
+                    <label htmlFor="file">Multiple files upload</label>
+
+                    <UploadComponent
+                      setFieldValue={setFieldValue}
+                      setImage={setImage}
                     />
-                    <TextField
-                      id="finder"
-                      name="finder"
-                      label="finder"
-                      value={values.finder}
-                      onChange={handleChange}
-                      onBlur={submitForm}
-                      error={touched.finder && Boolean(errors.finder)}
-                      helperText={touched.finder && errors.finder}
-                    />
-                  </Box>
-                  <Box
-                    sx={{
-                      display: "grid",
-                      gridTemplateColumns: { sm: "1fr 1fr" },
-                      gap: 2,
-                      marginTop: 2,
-                    }}
-                  >
-                    <SelectField
-                      name="gender"
-                      options={optionsGenre}
-                      placeholder={"Gender"}
-                      submitForm={submitForm}
-                    />
-                    <SelectField
-                      name="gender"
-                      options={optionsGenre}
-                      placeholder={"Gender"}
-                      submitForm={submitForm}
-                    />
-                  </Box>
-                  <SelectFieldMulty
-                    name="gender"
-                    options={optionsGenre}
-                    placeholder={"Gender"}
-                    submitForm={submitForm}
-                  />
+                    {values.files &&
+                      values.files.map((file, i) => (
+                        <li key={i}>
+                          {`File:${file.name} Type:${file.type} Size:${file.size} bytes`}{" "}
+                        </li>
+                      ))}
+                  </div>
+                  {/* dropzone */}
+                  {values.files && (
+                    <Box
+                      sx={{
+                        display: "grid",
+                        gridTemplateColumns: { sm: "1fr 1fr" },
+                        gap: 2,
+                        marginTop: 2,
+                      }}
+                    >
+                      <TextField
+                        id="songName"
+                        name="songName"
+                        label="songName"
+                        value={values.songName}
+                        onChange={handleChange}
+                        placeholder={values.files[0].name}
+                        error={touched.songName && Boolean(errors.songName)}
+                        helperText={touched.songName && errors.songName}
+                      />
+                      <TextField
+                        id="songArtist"
+                        name="songArtist"
+                        label="songArtist"
+                        value={values.songArtist}
+                        onChange={handleChange}
+                        // onBlur={submitForm}
+                        error={touched.songArtist && Boolean(errors.songArtist)}
+                        helperText={touched.songArtist && errors.songArtist}
+                      />
+                      <TextField
+                        id="songAlbum"
+                        name="songAlbum"
+                        label="songAlbum"
+                        value={values.songAlbum}
+                        onChange={handleChange}
+                        // onBlur={submitForm}
+                        error={touched.songAlbum && Boolean(errors.songAlbum)}
+                        helperText={touched.songAlbum && errors.songAlbum}
+                      />
+                      <SelectField
+                        name="gender"
+                        options={optionsGenre}
+                        placeholder={"Gender"}
+                        // submitForm={submitForm}
+                      />
+                    </Box>
+                  )}
                 </div>
                 <Button
                   type="submit"
                   variant="contained"
-                  disabled={isValidating || !isValid}
+                  disabled={isValidating || !isValid || !values.files}
                 >
-                  Submit
+                  Submit Details
                 </Button>
               </Form>
             )}
           </Formik>
-        </div>
-      </section>
-      {signUpError && (
-        <section className="row row-cols-1 mb-3 border py-3 bg-light">
-          <div className="col">
-            <h2 className="h2 h2SignUp">Something went wrong</h2>
-            <hr />
-            <p className="mt-3">{signUpError}</p>
-          </div>
-        </section>
-      )}
-      <section className="row row-cols-1 mb-5">
-        <div className="col">
-          <p className="accExists">You already have a Musikfy account?</p>
-          <Link to="/login">Log in</Link>
-          <hr />
-        </div>
-        <div className="col">
-          <Link to="/reset-password">Forgot your password?</Link>
         </div>
       </section>
     </>
