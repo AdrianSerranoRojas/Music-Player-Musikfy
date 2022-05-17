169a232b227d3bf7abda7070cc390b99b8c14664
JSSatorres <juansataz@gmail.com>
2022-05-17 11:21:12 +0200

    Laravel Server WIP

---
+++ serverLaravel/src/routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\songController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::controller(songController::class)->group(function(){
+    Route::get("songs","index");
+    Route::post("song","store");
+    Route::get("song/{id}","show");
+    Route::put("song/{id}","update");
+    Route::get("song/{id}","destroy");
+
+});
