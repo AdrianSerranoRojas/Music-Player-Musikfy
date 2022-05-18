ab7e620ec7d20d55de4918c1f29340a2a2b92144
JSSatorres <juansataz@gmail.com>
2022-05-18 19:28:26 +0200

    data websrapping ok

--- Frontend/src/Pages/ListWebScrap/ListWebScrap.tsx
+++ Frontend/src/Pages/ListWebScrap/ListWebScrap.tsx
@@ -1,36 +1,39 @@
-import { useState, useContext, useEffect } from "react";
+import { useEffect, useState } from "react";
 // import { Link } from "react-router-dom";
 // import "./Login.scss";
 // import { FcGoogle } from "react-icons/fc";
 import { Button } from "@mui/material";
 import withLayout from "../../hoc/withLayout";
-
+import { Spinner } from "../../components/Spinner/Spinner";
 
 function ListWebScrap() {
-
-  const [dataFetech40list, setDataFetech40list] = useState(null);
+  const [dataFetech40list, setDataFetech40list] = useState([]);
 
   const handleFetchWebScrap = async () => {
     await fetch("http://localhost:4000/40lists")
       .then((response) => response.json())
       .then((data) => {
-        setDataFetech40list(data);
+        console.log(data);
+        setDataFetech40list(data.data);
+        console.log(dataFetech40list);
       });
   };
-
-  // useEffect(() => {}, []);
-
+  useEffect(() => {
+    handleFetchWebScrap();
+  },[]);
   return (
     <div className="container p-4 mt-5">
       <h2>listWebScrap</h2>
-      <Button onClick={handleFetchWebScrap}> fetch listWebScrap</Button>
+      {/* <Button onClick={handleFetchWebScrap}> fetch listWebScrap</Button> */}
       {/* !dataSa && <div>no existo</div>) */}
 
-      {/* {(dataFetech40list ==! null)
-        ? dataFetech40list.map((song, index) => {
-            return <div> osita </div>;
-          })
-        : null} */}
+      {dataFetech40list?.length > 0 ? (
+        dataFetech40list.map((song, index) => {
+          return <div key={index}> {song} </div>;
+        })
+      ) : (
+        <Spinner />
+      )}
     </div>
   );
 }
